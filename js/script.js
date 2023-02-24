f03d810b4d31d518342861149d6964c44bd5bcec
Nahid <nhnahid.du@gmail.com>
2023-02-24 21:59:25 +0600

    3rd  commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@ const loadAllNewsCategory = () =>{
     const url = ` https://openapi.programming-hero.com/api/news/categories`;
     fetch(url)
     .then(res => res.json())
-    .then(data =>console.log(data.data.news_category))
+    .then(data =>displayAllNewsCategory(data.data.news_category))
 }
+const displayAllNewsCategory = categories =>{
+    // console.log(categories);
+    const categoryContainer = document.getElementById('heading');
+     categories.forEach(category => {
+        //   console.log(category);
+        const categoryList = document.createElement('button');
+        categoryList.addEventListener("click", function(){
+            console.log(category.category_id);
+        })
+        categoryList.classList.add( 'list','btn' , 'btn-light');
+        categoryList.innerText = category.category_name;
+        categoryContainer.appendChild(categoryList);
+     });
+};
+
+
 loadAllNewsCategory();
